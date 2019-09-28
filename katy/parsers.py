f35a7164b3a18a3cfe87fb557beffd3aef4e5600
Noel Lee <noel@gluehome.com>
2019-09-29 02:30:16 +0800

    Fixed course name hyphen bug

--- katy/parsers.py
+++ katy/parsers.py
@@ -73,7 +73,7 @@ class TimetableParser:
     @staticmethod
     def _parse_course(course_id_elem: Tag):
         course_id = course_id_elem.get_text()
-        course_name = str(course_id_elem.next_sibling).replace(' - ', '')
+        course_name = str(course_id_elem.next_sibling).replace(' - ', '', 1)
         # todo: support all levels
         level = 3 if 'style' in course_id_elem.find_parent('td').attrs else 1
         return Course(id=course_id, name=course_name, level=level)
